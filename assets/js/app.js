414ca67ab423b3c403fe309706d5c1f24bb9e26a
Fausto Rendon <faustor001@gmail.com>
2018-01-01 20:29:17 -0800

    update profile and contact page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,76 @@
 // Wait for DOM to load.
 $(function() {
-  $(".card img").on("click", function(event) {
-    var currentItem = $(this).data("profileItem");
 
+  //toggle hide/unhide project information when the project image is clicked
+  $(".card img").on("click", function(event) {
+
+    var currentItem = $(this).data("profileItem");
     var siblingCardBody = $(this).next();
 
-    //toggle hide/unhide project information when clicked
     if (siblingCardBody.hasClass("collapse")) {
       siblingCardBody.removeClass("collapse");
     } else {
       siblingCardBody.addClass("collapse");
     }
 
-
   });
+
+  //animate main page text
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+  };
 
 });
